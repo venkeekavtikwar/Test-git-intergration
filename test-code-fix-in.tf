6f2ea3b4320fbffa8b364d987b48a0b4326a25d1
venkeekavtikwar <51167388+venkeekavtikwar@users.noreply.github.com>
2024-09-25 10:38:40 +0530

    Update test-code-fix-in.tf

--- test-code-fix-in.tf
+++ test-code-fix-in.tf
@@ -13,9 +13,12 @@ resource "aws_s3_bucket" "my_bucket" {
 }
 
 resource "aws_s3_bucket_public_access_block" "block_public_acls" {
-    bucket = aws_s3_bucket.my_bucket
-    block_public_acls = true
-    block_public_policy = true
-    ignore_public_acls = true
-    restrict_public_buckets = true
+  bucket = aws_s3_bucket.my_bucket.id
+
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls  = true
+  restrict_public_buckets = true
+
+  depends_on = [aws_s3_bucket.my_bucket]
 }
