cbb2de565b73612a8953a67ceefc8158f443db21
venkeekavtikwar <51167388+venkeekavtikwar@users.noreply.github.com>
2024-09-25 10:09:31 +0530

    Update-test-code-fix.tf

--- test-code-fix.tf
+++ test-code-fix.tf
@@ -11,3 +11,11 @@ resource "aws_s3_bucket" "my_bucket" {
     Environment = "Dev"
   }
 }
+
+resource "aws_s3_bucket_public_access_block" "block_public_acls" {
+    bucket = aws_s3_bucket.my_bucket
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+}
