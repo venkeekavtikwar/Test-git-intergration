5bab206e85ceb15c502e2abeb3f58bbd39fce706
venkeekavtikwar <51167388+venkeekavtikwar@users.noreply.github.com>
2025-04-17 11:56:49 +0530

    Update venkee-s3-new-Sept-17.tf

--- venkee-s3-new-Sept-17.tf
+++ venkee-s3-new-Sept-17.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region = "us-west-2"  # Replace with your preferred region
 }
 
+provider "aws" {
+  region = "us-west-2"  # Replace with your preferred region
+}
+
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-unique-bucket-name"  # Replace with a unique bucket name
   acl    = "private"
